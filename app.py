4336ac6dbee3e13d9e6e0ea1090db4ebaf397966
chenp6 <52535982+chenp6@users.noreply.github.com>
2020-04-20 14:15:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError, LineBotApiError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
@@ -11,8 +11,13 @@ import random,os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('S7X9UWoHaIh4oXILrCZTSjUj4ngwiZSDt+WMLA/VmA6UG5dxOxRGF8ODOq4H9ZuOYgrTTaDQYN2B6iBIaXEQPwKoJt3uit3anqklTWw1N7MyL4XKOTEsqFn+SvuPv58i31npmgTv5ykduY/T2AaMUAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('7b71a98cfbf44c0ea7f3eb1429148802')
+accessToken = 'S7X9UWoHaIh4oXILrCZTSjUj4ngwiZSDt+WMLA/VmA6UG5dxOxRGF8ODOq4H9ZuOYgrTTaDQYN2B6iBIaXEQPwKoJt3uit3anqklTWw1N7MyL4XKOTEsqFn+SvuPv58i31npmgTv5ykduY/T2AaMUAdB04t89/1O/w1cDnyilFU='
+channelSecret = '7b71a98cfbf44c0ea7f3eb1429148802'
+userID ='Ubdf70dbd043a808bbdc15302999fbca1'
+
+
+line_bot_api = LineBotApi(accessToken)
+handler = WebhookHandler(channelSecret)
 
 
 @app.route("/callback", methods=['POST'])
@@ -43,6 +48,10 @@ def handle_message(event):
       line_bot_api.reply_message(event.reply_token,TextSendMessage(text="第%d項優惠"%num))
     elif msg =="誰是正妹?":
       line_bot_api.reply_message(event.reply_token,TextSendMessage(text="你就是正妹"))
+    else:
+        line_bot_api.push_message(userID,1,2)
+                
+        
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
